06bf8ea0829bd08d374f657cfc540bb845bdc5e0
michaelyu77777 <michaelyu77777@gmail.com>
2020-10-29 11:17:43 +0800

    #加入寫LOG機制

--- 11_OK_讀取日打卡紀錄+寫入mongoDB(指定期間所有)/main.go
+++ 11_OK_讀取日打卡紀錄+寫入mongoDB(指定期間所有)/main.go
@@ -11,6 +11,9 @@ import (
 	"time"
 
 	//"labix.org/v2/mgo"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
 	"gopkg.in/mgo.v2"
 
 	"golang.org/x/text/encoding/traditionalchinese" // 繁體中文編碼
@@ -40,13 +43,65 @@ func main() {
 	ImportDailyRecord()
 }
 
+//Log檔
+var log_info *logrus.Logger
+var log_err *logrus.Logger
+
 /*
  * 初始化配置
  */
 func init() {
+
+	fmt.Println("執行init()初始化")
+
+	/**設定LOG檔層級與輸出格式*/
+	//使用Info層級
+	path := "./log/info/info"
+	writer, _ := rotatelogs.New(
+		path+".%Y%m%d%H",                            // 檔名格式
+		rotatelogs.WithLinkName(path),               // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(10080*time.Minute),    // 文件最大保存時間(保留七天)
+		rotatelogs.WithRotationTime(60*time.Minute), // 日誌切割時間間隔(一小時存一個檔案)
+	)
+
+	// 設定LOG等級
+	pathMap := lfshook.WriterMap{
+		logrus.InfoLevel: writer,
+		//logrus.PanicLevel: writer, //若執行發生錯誤則會停止不進行下去
+	}
+
+	log_info = logrus.New()
+	log_info.Hooks.Add(lfshook.NewHook(pathMap, &logrus.JSONFormatter{})) //Log檔綁訂相關設定
+
+	fmt.Println("結束Info等級設定")
+
+	//Error層級
+	path = "./log/err/err"
+	writer, _ = rotatelogs.New(
+		path+".%Y%m%d%H",                            // 檔名格式
+		rotatelogs.WithLinkName(path),               // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(10080*time.Minute),    // 文件最大保存時間(保留七天)
+		rotatelogs.WithRotationTime(60*time.Minute), // 日誌切割時間間隔(一小時存一個檔案)
+	)
+
+	// 設定LOG等級
+	pathMap = lfshook.WriterMap{
+		//logrus.InfoLevel: writer,
+		logrus.ErrorLevel: writer,
+		//logrus.PanicLevel: writer, //若執行發生錯誤則會停止不進行下去
+	}
+
+	log_err = logrus.New()
+	log_err.Hooks.Add(lfshook.NewHook(pathMap, &logrus.JSONFormatter{})) //Log檔綁訂相關設定
+
+	fmt.Println("結束Error等級設定")
+	log_info.Info("結束Error等級設定")
+
+	/*json設定檔*/
 	file, _ := os.Open("config.json")
 	buf := make([]byte, 2048)
 
+	//將設定讀到config變數中
 	n, _ := file.Read(buf)
 	fmt.Println(string(buf))
 	err := json.Unmarshal(buf[:n], &config)
@@ -56,7 +111,7 @@ func init() {
 	}
 }
 
-//配置
+// 設定檔
 type Config struct {
 	MongodbServer string
 	StartDate     string // 讀檔開始日
@@ -92,24 +147,29 @@ func addDailyRecordForManyDays(chanDailyRecord chan<- DailyRecord) {
 
 	/** 取得開始日期 **/
 	stringStartDate := config.StartDate
+
 	// 取出年月日
 	startYear, _ := strconv.Atoi(stringStartDate[0:4])
 	startMonth, _ := strconv.Atoi(stringStartDate[4:6])
 	startDay, _ := strconv.Atoi(stringStartDate[6:8])
+
 	// 轉成time格式
 	dateStart := time.Date(startYear, time.Month(startMonth), startDay, 0, 0, 0, 0, time.Local)
+	fmt.Println("檔案開始日期:", dateStart)
+	log_info.Info("檔案開始日期: ", dateStart)
 
 	/** 取得結束日期 **/
 	stringEndDate := config.EndDate
+
 	// 取出年月日
 	endYear, _ := strconv.Atoi(stringEndDate[0:4])
 	endMonth, _ := strconv.Atoi(stringEndDate[4:6])
 	endDay, _ := strconv.Atoi(stringEndDate[6:8])
+
 	// 轉成time格式
 	dateEnd := time.Date(endYear, time.Month(endMonth), endDay, 0, 0, 0, 0, time.Local)
-
-	fmt.Println("開始日期:", dateStart)
-	fmt.Println("結束日期:", dateEnd)
+	fmt.Println("檔案結束日期:", dateEnd)
+	log_info.Info("檔案結束日期: ", dateEnd)
 
 	// for myTime := time.Date(2020, 1, 1, 9, 0, 0, 0, time.Local); myTime != time.Date(2021, 1, 1, 9, 0, 0, 0, time.Local); myTime = myTime.AddDate(0, 0, 1) {
 	// }
@@ -119,6 +179,7 @@ func addDailyRecordForManyDays(chanDailyRecord chan<- DailyRecord) {
 
 		// 檔案名稱(年月日)
 		fileName := "Rec" + myTime.Format("20060102") + ".csv"
+		log_info.Info("讀取檔名: ", fileName)
 		fmt.Println("讀取檔名:", fileName)
 
 		// 打開每日打卡紀錄檔案(本機要先登入過目的地磁碟機才能正常運作)
@@ -126,14 +187,20 @@ func addDailyRecordForManyDays(chanDailyRecord chan<- DailyRecord) {
 		file, err := os.Open("\\\\leapsy-nas3\\CheckInRecord\\" + fileName)
 
 		if err != nil {
-			fmt.Println("打開文件失敗", err)
+			fmt.Println("打開檔案失敗", err)
+			log_err.WithFields(logrus.Fields{
+				"trace":    "trace-0001",
+				"err":      err,
+				"fileName": fileName,
+			}).Error("打開檔案失敗")
 			return
 		}
 
 		// 最後回收資源
 		defer file.Close()
 
-		fmt.Println("讀取文件")
+		fmt.Println("開始讀取檔案")
+		log_info.Info("開始讀取檔案: ", fileName)
 
 		// 讀檔
 		reader := csv.NewReader(file)
@@ -147,20 +214,45 @@ func addDailyRecordForManyDays(chanDailyRecord chan<- DailyRecord) {
 			if err == io.EOF {
 
 				fmt.Println(fileName, "此份檔案讀取完成")
+				log_info.Info("此份檔案讀取完成: ", fileName)
+
 				break
 			} else if err != nil {
+
 				close(chanDailyRecord)
+
+				fmt.Println("關閉channel")
 				fmt.Println("Error:", err)
+
+				log_err.WithFields(logrus.Fields{
+					"trace":    "trace-0002",
+					"err":      err,
+					"fileName": fileName,
+				}).Error("關閉channel")
+
 				break
 			}
 
 			// 處理Name編碼問題: 將繁體(Big5)轉成 UTF-8，儲存進去才正常
+			log_info.Info("轉成UTF8配合繁體")
 			big5Name := line[1]                                             // Name(Big5)
 			utf8Name, _, _ := transform.String(big5ToUTF8Decoder, big5Name) // 轉成 UTF-8
 			//fmt.Println(utf8Name) // 顯示"名字"
 
-			dailyrecord := DailyRecord{line[0], utf8Name, line[2], line[3], line[4], line[5]} // 建立每筆DailyRecord物件
-			chanDailyRecord <- dailyrecord                                                    // 存到channel裡面
+			// 建立每筆DailyRecord物件
+			log_info.WithFields(logrus.Fields{
+				"line[0]":  line[0],
+				"utf8Name": utf8Name,
+				"line[2]":  line[2],
+				"line[3]":  line[3],
+				"line[4]":  line[4],
+				"line[5]":  line[5],
+			}).Info("dailyrecord")
+
+			dailyrecord := DailyRecord{line[0], utf8Name, line[2], line[3], line[4], line[5]}
+
+			// 存到channel裡面
+			chanDailyRecord <- dailyrecord
 		}
 	}
 
@@ -175,15 +267,26 @@ func insertDailyRecord(chanDailyRecord <-chan DailyRecord, dones chan<- struct{}
 
 	session, err := mgo.Dial(config.MongodbServer)
 	if err != nil {
-		fmt.Println("錯誤")
+		fmt.Println("打卡紀錄插入錯誤(insertDailyRecord)")
+
+		log_err.WithFields(logrus.Fields{
+			"trace": "trace-0003",
+			"err":   err,
+		}).Error("打卡紀錄插入錯誤(insertDailyRecord)")
+
 		panic(err)
 		return
 	}
+
 	defer session.Close()
+
+	log_info.Info("DB:leapsy_env, Collection:dailyRecord_real")
 	c := session.DB("leapsy_env").C("dailyRecord_real")
 
 	for dailyrecord := range chanDailyRecord {
-		fmt.Println("插入：", dailyrecord)
+		fmt.Println("插入一筆打卡資料：", dailyrecord)
+		log_info.Info("插入一筆打卡資料:", dailyrecord)
+
 		c.Insert(&dailyrecord)
 	}
 
