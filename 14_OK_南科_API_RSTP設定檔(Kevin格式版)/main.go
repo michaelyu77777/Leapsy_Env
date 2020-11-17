6617f074f52c48b9b52d282337d5dd7389dc3820
michaelyu77777 <69225666+michaelyu77777@users.noreply.github.com>
2020-11-17 14:55:26 +0800

    #新增14_OK_南科API_RSTP設定檔(Kevin格式版)
    #新增14_OK_南科API_RSTP設定檔(Kevin格式版)

--- 14_OK_南科_API_RSTP設定檔(Kevin格式版)/main.go
+++ 14_OK_南科_API_RSTP設定檔(Kevin格式版)/main.go
@@ -4,27 +4,49 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"./objects"
+	"github.com/sirupsen/logrus"
 )
 
+var log_info logrus.Logger
+var log_err logrus.Logger
+var config objects.Config
+
 func main() {
-	router := mux.NewRouter() // 新路由
+
+	// 建立新路由
+	router := mux.NewRouter()
+
+	// 建立路由路徑與Handler
 	router.HandleFunc(`/SouthSience/RtspConfig/query`, dailyAPIHandler)
 
+	log_info.WithFields(logrus.Fields{}).Info("建立路由路徑")
+
+	//讀入config
+	readConfig()
+
+	// 建立Server
 	apiServerPointer := &http.Server{
-		Addr:           ":8007",
+		//Addr:           ":8007",
+		Addr:           config.portofAPI,
 		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
-	} // 設定伺服器
+	}
+
+	log_info.WithFields(logrus.Fields{}).Info("建立Server")
 
 	log.Fatal(apiServerPointer.ListenAndServe())
 
@@ -34,42 +56,39 @@ func dailyAPIHandler(w http.ResponseWriter, r *http.Request) {
 
 	mongoClientPointer, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(`mongodb://localhost:27017`)) // 連接預設主機
 
+	log_info.WithFields(logrus.Fields{}).Info("建立資料庫連線")
+
 	if nil != err {
-		fmt.Fprintf(w, "no data") // 寫入回應
+		fmt.Fprintf(w, "建立資料庫連線") // 寫入回應
+		log_err.WithFields(logrus.Fields{"err": err}).Error("建立資料庫連線錯誤")
 		return
 	}
 
-	cursor, err := mongoClientPointer.
+	content, err := mongoClientPointer.
 		Database(`SouthernScience`).
 		Collection(`rtsp_config`).
 		Find(context.TODO(), bson.M{"EnableAudioStream": false}) //find all
-	//Find(context.TODO(), bson.M{"time": bson.M{`$gt`: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local), `$lt`: time.Date(2020, 12, 1, 0, 0, 0, 0, time.Local)}}) //時間要大於某時間 並且小於某時間
 
-	// cursor, err := mongoClientPointer.
-	// 	Database(`Leapsy-Environmental-Control-Database`).
-	// 	Collection(`second-records`).
-	// 	Find(context.TODO(), bson.M{"time": bson.M{`$gt`: time.Date(2020, 1, 1, 0, 0, 0, 0, time.Local), `$lt`: time.Date(2020, 12, 1, 0, 0, 0, 0, time.Local)}}) //時間要大於某時間 並且小於某時間
+	log_info.WithFields(logrus.Fields{}).Info("找colleciton全部")
 
 	if nil != err {
-		fmt.Fprintf(w, "err") // 寫入回應
+		fmt.Fprintf(w, "查Collection錯誤") // 寫入回應
+		log_err.WithFields(logrus.Fields{"err": err}).Error("查Collection錯誤")
 		return
 	}
 
-	fmt.Println("我是Cursor", cursor)
+	var results []objects.RtspConfig
 
-	var results []RtspConfig
-	//var results []Data
+	for content.Next(context.TODO()) { // 針對每一紀錄
 
-	for cursor.Next(context.TODO()) { // 針對每一紀錄
+		var rtspConfig objects.RtspConfig
 
-		var rtspConfig RtspConfig
-		//var data Data
-
-		err = cursor.Decode(&rtspConfig) // 解析紀錄
+		err = content.Decode(&rtspConfig) // 解析紀錄
 
 		if nil != err {
-			fmt.Fprintf(w, "解析沒有資料no data") // 寫入回應
-			fmt.Println("錯誤訊息:", err)
+			fmt.Fprintf(w, "解析錯誤") // 寫入回應
+			log_err.WithFields(logrus.Fields{"err": err}).Error("解析錯誤")
+
 			return
 		}
 
@@ -80,10 +99,38 @@ func dailyAPIHandler(w http.ResponseWriter, r *http.Request) {
 	jsonBytes, err := json.Marshal(results) // 轉成JSON
 
 	if nil != err {
-		fmt.Fprintf(w, "no data") // 寫入回應
+		fmt.Fprintf(w, "轉成JSON錯誤") // 寫入回應
+		log_err.WithFields(logrus.Fields{"err": err}).Error("轉成JSON錯誤")
 		return
 	}
 
 	fmt.Fprintf(w, "%s", string(jsonBytes)) // 寫入回應
 
+}
+
+// 讀設定檔
+func readConfig() {
+
+	File, err := os.Open("config.ini")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("成功開啟 config.ini")
+	defer File.Close()
+
+	byteValue, _ := ioutil.ReadAll(File)
+
+	//將json設定轉存入變數
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		panic(err)
+
+		log_err.WithFields(logrus.Fields{
+			"trace": "trace-0001",
+			"err":   err,
+		}).Error("將設定讀到config變數中失敗")
+
+		fmt.Println(err)
+	}
 }
